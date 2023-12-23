c0d48b42b17354c24610b4b8d973d72f7f659165
HülyaÖğüt <99360087+Hly-1038@users.noreply.github.com>
2023-12-23 16:10:46 +0300

    Add files via upload

---
+++ React Otobiografi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function App(props) {
+  return (
+    <div className='App'>
+      <h1>Hülya Öğüt</h1>
+      <h2>Konya Gıda ve Tarım Üniversitesi Bilgisayar Mühendisliği bölümünden 2023 yılında mezun oldum. 
+      Şu anda Konya Büyükşehir Belediyesinde Sistem ve network mühendisi olarak çalışıyorum.</h2>
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
